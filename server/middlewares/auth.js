6167f6d23d2042ea227832d7ad2ce0e2d48ac296
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-04-29 00:31:40 +0530

    Airflex - Dronza

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,7 +1,7 @@
 import errorHandler from '../utils/errorHandler.js'
 
 export const isAuthenticated = (req, res, next) => {
-    const token = req.cookies['connect.sid'];
+    const token = req.cookies('connect.sid');
     if (!token) {
         return next(new errorHandler('Not logged in', 401))
     }
