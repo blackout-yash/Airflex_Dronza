d69969633e308e42e1c89612947d4e4c0bde0a71
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-05-09 12:11:07 +0530

    test

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,9 +1,9 @@
 import errorHandler from '../utils/errorHandler.js'
 
 export const isAuthenticated = (req, res, next) => {
-    // console.log("auth1", req.session.user)
-    // console.log("auth2", req.user)
-    // console.log(req.isAuthenticated());
+    console.log("auth1", req.session.user)
+    console.log("auth2", req.user)
+    console.log(req.isAuthenticated());
     const token = req.cookies['connect.sid'];
     if (!token) {
         return next(new errorHandler('Not logged in', 401))
